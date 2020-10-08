ab25714f8e2e9c0699333d17a2eb144674847bd1
Snehil <snehilverma488@gmail.com>
2020-10-08 12:35:19 +0530

    [Snehil] Add : Ability To Add Multiple AddressBooks

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -8,7 +8,23 @@ namespace AddressBookSystem
         {
             Console.WriteLine("Welcome to Address Book Program");
             AddressBook addressBook = new AddressBook();
-            int choice;
+            int choice, choice2;
+            string bookName = "default";
+            Console.WriteLine("Would You Like To \n1.Work on default AddressBook \n2.Create New AddressBook");
+            choice2 = Convert.ToInt32(Console.ReadLine());
+            switch (choice2)
+            {
+                case 1:
+                    addressBook.AddAddressBook(bookName);
+                    Console.WriteLine($"Working On {bookName} AddressBook");
+                    break;
+                case 2:
+                    Console.WriteLine("Enter Name Of New Addressbook You want to create : ");
+                    bookName = Console.ReadLine();
+                    addressBook.AddAddressBook(bookName);
+                    Console.WriteLine($"Working On {bookName} AddressBook");
+                    break;
+            }
             do
             {
                 Console.WriteLine("Choose An Option \n1.Add New Contact \n2.Edit Existing Contact \n3.Delete A Contact \n4.View A Contact \n5.View All Contacts \n6.Exit Application\n");
@@ -31,26 +47,26 @@ namespace AddressBookSystem
                         Console.WriteLine("Enter Zip :");
                         int zip = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine("Enter Phone Number :");
-                        long phoneNumber = Convert.ToInt64(Console.ReadLine());
-                        addressBook.AddContact(firstName, lastName, address, city, state, email, zip, phoneNumber);
+                        long phoneNumber = long.Parse(Console.ReadLine());
+                        addressBook.AddContact(firstName, lastName, address, city, state, email, zip, phoneNumber, bookName);
                         break;
                     case 2:
                         Console.WriteLine("Enter First Name Of Contact To Edit :");
                         string nameToEdit = Console.ReadLine();
-                        addressBook.EditContact(nameToEdit);
+                        addressBook.EditContact(nameToEdit, bookName);
                         break;
                     case 3:
                         Console.WriteLine("Enter First Name Of Contact To Delete :");
                         string nameToDelete = Console.ReadLine();
-                        addressBook.DeleteContact(nameToDelete);
+                        addressBook.DeleteContact(nameToDelete, bookName);
                         break;
                     case 4:
                         Console.WriteLine("Enter First Name Of Contact To View :");
                         string nameToView = Console.ReadLine();
-                        addressBook.ViewContact(nameToView);
+                        addressBook.ViewContact(nameToView, bookName);
                         break;
                     case 5:
-                        addressBook.ViewContact();
+                        addressBook.ViewContact(bookName);
                         break;
                     case 6:
                         Console.WriteLine("Thank You For Using Address Book System.");
