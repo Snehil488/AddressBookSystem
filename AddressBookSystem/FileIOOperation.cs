367982d03a56abb6a9227cb82ba03c523058dfe9
Snehil <snehilverma488@gmail.com>
2020-10-26 14:09:34 +0530

    [Snehil] Add : Reading and Writing AddressBook to Text file using FileIO

---
+++ AddressBookSystem/FileIOOperation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class FileIOOperation
+    {
+        public void WriteToFile(Dictionary<string, AddressBook> addressBookDictionary)
+        {
+            using StreamWriter writer = new StreamWriter(@"C:\Users\Snehil\Desktop\Local Git Repo\AddressBookSystem\AddressBookSystem\AddressBookRecord.txt", true);
+            foreach (AddressBook addressBookobj in addressBookDictionary.Values)
+            {
+                foreach (Contact contact in addressBookobj.addressBook.Values)
+                {
+                    writer.WriteLine(contact.ToString());
+                }
+            }
+            Console.WriteLine("\nSuccessfully added to Text file.");
+            writer.Close();
+        }
+        public void ReadFromFile()
+        {
+            Console.WriteLine("Below are Contents of Text File");
+            string lines = File.ReadAllText(@"C:\Users\Snehil\Desktop\Local Git Repo\AddressBookSystem\AddressBookSystem\AddressBookRecord.txt");
+            Console.WriteLine(lines);
+        }
+    }
+}
